015367e64282ff1f3dd3a6326308fd7853109a89
XXAssDestroyerXX <72551615+XXAssDestroyerXX@users.noreply.github.com>
2021-09-03 12:59:55 +0200

    Weksowanie Push

---
+++ Wedkowanie.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Wędkujemy</title>
+
+</head>
+
+<body>
+<header>
+    <h1>Portal dla wedkarzy</h1>
+</header>
+<div id="lewy">
+<h3><strong>Ryby drapieżne naszych wód</strong></h3>
+<ul>
+    <?php 
+    
+    
+    
+    
+    ?>
+   
+        <span style="font-family: Verdana"><li>Szczupak, występowanie: stawy, rzeki</li></span>
+        <span style="font-family: Verdana"><li>Skandacz, występowanie: stawy, jeziora, rzeki</li></span>
+            <span style="font-family: Verdana"><li>Okon, występowanie: rzeki</li></span>
+                <span style="font-family: Verdana"><li>Sum, występowanie: jeziora, rzeki</li></span>
+                    <span style="font-family: Verdana"><li>Dorsz, występowanie: morza, oceany</li></span>
+</ul>
+</div>
+<div id="prawa">
+  <img src="https://4.bp.blogspot.com/-Rp_mnnLIiSk/XZFfXCku9_I/AAAAAAAAEAI/nKebrMZc_AYGur8QQ-uOWKXRwyjM19vZwCLcBGAsYHQ/s640/ryba1.jpg" alt="sum">
+  <a herf="url"><U>Pobierz Kwerendy</U></a><br>
+</div>
+
+<footer>
+    <p>stronę wykonał: Kacper Kamiński</p>
+</footer>
+
+</body>
+</html>

e9be3cf95d86b52a9ea4bbe971bd76cfa4e62a2d
XXAssDestroyerXX <72551615+XXAssDestroyerXX@users.noreply.github.com>
2021-10-19 19:58:57 +0200

    Dokończenie Egzaminu

--- EE.09-03-20.06/kalendarz.php
+++ EE.09-03-20.06/kalendarz.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,25 +8,58 @@
     <link rel="stylesheet" href="style5.css">
 </head>
 
+
+
 <body>
-
-    <div class=".gorny-lewy">
-        <img src="logo1.png" alt="Mój kalendarz">
+    <div id="A1">
+        <img src="kalendarz.png" alt="Mój kalendarz">
     </div>
-
-    <div class=".gorny-prawy">
+    <div id="A2">
         <h1>KALENDARZ</h1>
-        <p>miesiąc: lipiec, rok:2020</p>
+        <?php
+            $db = new mysqli('localhost', 'root', '', 'egzamin5');
+            $query = "SELECT miesiac, rok FROM `zadania` WHERE dataZadania = \"2020-07-01\";";
+            $result = $db->query($query);
+            while ($row = $result->fetch_assoc()){
+                $miesiac = $row['miesiac'];
+                $rok = $row['rok'];
+                echo "<h3>miesiąc: $miesiac, rok: $rok</h3>";
+            }
+            $db->close();
+        ?>
     </div>
-
-    <footer class="stopka">
-        <form action="kalendarz.php" method="POST">
-            <label for="wpis">dodaj wpis:<input type="text" name="wydarzenie"> <button type="submit">DODAJ</button></label>
-            <br>
-            <br>
-            <p>Kacper Kamiński 4GH</p>
+    <main>
+        <?php
+            $db = new mysqli('localhost', 'root', '', 'egzamin5');
+            $query = "SELECT dataZadania, wpis FROM `zadania` WHERE miesiac = 'lipiec';";
+            $result = $db->query($query);
+            while ($row = $result->fetch_assoc()) {
+                $data = $row['dataZadania'];
+                $wpis = $row['wpis'];
+                echo '<div>';
+                echo "<h5>$data</h5>";
+                echo "<p>$wpis</p>";
+                echo '</div>';
+            }
+            $db->close();
+        ?>
+    </main>
+    <?php
+        $db = new mysqli('localhost', 'root', '', 'egzamin5');
+        if (isset($_POST['wydarzenie'])) {
+            $wydarzenie = $_POST['wydarzenie'];
+            $query = "UPDATE zadania SET wpis = '$wydarzenie' WHERE dataZadania = '2020-07-13'";
+            $db->query($query);
+            $db->close();
+        }
+    ?>
+    <footer>
+        <form action="kalendarz.php" method="post">
+            <label for="wydarzenie">dodaj wpis:</label>
+            <input type="text" name="wydarzenie">
+            <button type="submit">DODAJ</button><br>
+            <p>Stronę wykonał: 02291907596</p>
         </form>
     </footer>
-
 </body>
 </html>

1377b6ff245e09f0287b241c758c15a57592c2d7
XXAssDestroyerXX <72551615+XXAssDestroyerXX@users.noreply.github.com>
2021-10-23 08:43:31 +0200

    naprawa

--- EE.09-03-20.06/kalendarz.php
+++ EE.09-03-20.06/kalendarz.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mój kalendarz</title>
+    <title>Moj kalendarz</title>
     <link rel="stylesheet" href="style5.css">
 </head>
 
@@ -12,14 +12,17 @@
 
 <body>
     <div id="A1">
-        <img src="kalendarz.png" alt="Mój kalendarz">
+        <img src="kalendarz.png" alt="Moj kalendarz">
     </div>
+
+
     <div id="A2">
         <h1>KALENDARZ</h1>
-        <?php
-            $db = new mysqli('localhost', 'root', '', 'egzamin5');
+        <?php //mysql
+            $db = new mysqli('localhost','root','','egzamin5');
             $query = "SELECT miesiac, rok FROM `zadania` WHERE dataZadania = \"2020-07-01\";";
             $result = $db->query($query);
+
             while ($row = $result->fetch_assoc()){
                 $miesiac = $row['miesiac'];
                 $rok = $row['rok'];
@@ -28,37 +31,43 @@
             $db->close();
         ?>
     </div>
+
+
     <main>
-        <?php
-            $db = new mysqli('localhost', 'root', '', 'egzamin5');
+        <?php //mysql
+            $db = new mysqli('localhost','root','','egzamin5');
             $query = "SELECT dataZadania, wpis FROM `zadania` WHERE miesiac = 'lipiec';";
             $result = $db->query($query);
             while ($row = $result->fetch_assoc()) {
-                $data = $row['dataZadania'];
-                $wpis = $row['wpis'];
-                echo '<div>';
-                echo "<h5>$data</h5>";
-                echo "<p>$wpis</p>";
-                echo '</div>';
+            $data = $row['dataZadania'];
+            $wpis = $row['wpis'];
+            echo '<div>';
+            echo "<h5>$data</h5>";
+            echo "<p>$wpis</p>";
+            echo '</div>';
             }
             $db->close();
         ?>
     </main>
-    <?php
-        $db = new mysqli('localhost', 'root', '', 'egzamin5');
-        if (isset($_POST['wydarzenie'])) {
+
+
+        <?php //mysql
+            $db = new mysqli('localhost','root','','egzamin5');
+            if (isset($_POST['wydarzenie'])) {
             $wydarzenie = $_POST['wydarzenie'];
             $query = "UPDATE zadania SET wpis = '$wydarzenie' WHERE dataZadania = '2020-07-13'";
             $db->query($query);
             $db->close();
-        }
-    ?>
+            }
+        ?>
+
+
     <footer>
         <form action="kalendarz.php" method="post">
-            <label for="wydarzenie">dodaj wpis:</label>
+            <label for="wydarzenie">Dodaj wpis</label>
             <input type="text" name="wydarzenie">
-            <button type="submit">DODAJ</button><br>
-            <p>Stronę wykonał: 02291907596</p>
+            <button type="submit">Dodaj</button><br>
+            <p>Strone wykonał: Kacper Kamiński</p>
         </form>
     </footer>
 </body>
